28073767e7ff86dc278e69db1857b868d7e6d1d8
Israel Valdez <israel.valdez@teschglobal.com>
2016-05-16 16:27:58 -0500

    patient details panel

--- public/scripts/loadXML.js
+++ public/scripts/loadXML.js
@@ -26,6 +26,66 @@ var Panel=React.createClass(
     }
 });
 
+class PatientDetails extends React.Component
+{
+    render()
+    {
+        var patientRole=this.props.patientRole;
+        var patientName=patientRole.patient.name;
+        var name=buildName(patientName);
+        var dob=moment(patientRole.patient.birthTime.value).format("LL");
+        var guardian=patientRole.patient.guardian;
+        var address=buildAddress(patientRole.addr);
+        var contact=buildTelecom(patientRole.telecom);
+        var guardianName, guardianTag, addressTag, contactTag;
+
+        if(guardian)
+        {
+            var guardianEntity=guardian.guardianPerson ? guardian.guardianPerson : guardian.guardianOrganization;
+            guardianName=buildName(guardianEntity.name);
+        }
+
+        if(guardianName)
+            guardianTag=(
+                <div>
+                    <h6>Guardian</h6>
+                    <p>{guardianName}</p>
+                </div>
+            );
+
+        if(address)
+            addressTag=(
+                <div>
+                    <h6>Address</h6>
+                    {address}
+                </div>
+            );
+
+        if(contact)
+            contactTag=(
+                <div>
+                    <h6>Contact</h6>
+                    {contact}
+                </div>
+            );
+
+        return(
+            <div className="col-lg-4 col-md-4 col-sm-4 mb">
+                <div className="white-panel pn">
+                    <div className="white-header">
+                        <h4>{name}</h4>
+                    </div>
+                    <h6>Birthdate</h6>
+                    <p>{dob}</p>
+                    {guardianTag}
+                    {addressTag}
+                    {contactTag}
+                </div>
+            </div>
+        );
+    }
+}
+
 class XMLForm extends React.Component
 {
     render()
@@ -68,9 +128,10 @@ class XMLForm extends React.Component
             success: function(data)
             {
                 console.log("success: "+data);
-
+                debugvar=data;
                 var components = data.ClinicalDocument.component.structuredBody.component;
                 var title = data.ClinicalDocument.title;
+                var patientRole=data.ClinicalDocument.recordTarget.patientRole;
                 /*var titles = new Array();
 
                 for(var i = 0; i < components.length; i++){
@@ -79,14 +140,13 @@ class XMLForm extends React.Component
 
                 }*/
 
-                $('#myModal').modal('toggle');
+                $("#myModal").modal("toggle");
 
-                ReactDOM.render(<Panel data={components}/>, document.getElementById('panels'));
-
+                ReactDOM.render(<Panel data={components}/>, document.getElementById("panels"));
+                ReactDOM.render(<PatientDetails patientRole={patientRole}/>, document.getElementById("patientDetails"));
             },
             error: function(err)
             {
-                debugvar=err;
                 console.log("error: "+err);
             },
             processData: false,
@@ -95,4 +155,98 @@ class XMLForm extends React.Component
     }
 }
 
-ReactDOM.render(<XMLForm/>, document.getElementById('modal-container'));
+/*** Utility functions ***/
+// Extract just the text inside a node when there might be additional attributes (in that case the text content is inside "$")
+function getNodeText(nodeElement)
+{
+    if(!nodeElement)
+        return null;
+    if(typeof nodeElement==="string" || nodeElement instanceof String)
+        return nodeElement;
+    return nodeElement.$
+}
+
+function buildName(nameNode)
+{
+    if(!nameNode)
+        return null;
+
+    var name="";
+
+    // use the first occurrence of name if more than one is found
+    if(Array.isArray(nameNode))
+        nameNode=nameNode[0];
+
+    if(nameNode.prefix)
+        name=getNodeText(nameNode.prefix)+" ";
+
+    if(Array.isArray(nameNode.given))
+    {
+        for(var i=0; i<nameNode.given.length; i++)
+            name+=getNodeText(nameNode.given[i])+" ";
+    }
+    else
+        name+=getNodeText(nameNode.given)+" ";
+
+    name+=getNodeText(nameNode.family);
+
+    if(nameNode.suffix)
+        name+=", "+getNodeText(nameNode.suffix);
+
+    return name;
+}
+
+function buildAddress(addressNode)
+{
+    if(!addressNode)
+        return null;
+
+    var street, city, state, zip;
+
+    // if more than one address, use the first one
+    if(Array.isArray(addressNode))
+        addressNode=addressNode[0];
+
+    street=getNodeText(addressNode.streetAddressLine);
+    city=getNodeText(addressNode.city);
+    state=getNodeText(addressNode.state);
+    zip=getNodeText(addressNode.postalCode);
+
+    return(
+        <div>
+            <p>{street}</p>
+            <p>{city}, {state} {zip}</p>
+        </div>
+    );
+}
+
+//contact information
+function buildTelecom(telecomNode)
+{
+    if(!telecomNode)
+        return null;
+
+    if(Array.isArray(telecomNode))
+    {
+        var contact=[];
+
+        for(var i=0; i<telecomNode.length; i++)
+        {
+            var value=telecomNode[i].value;
+            contact.push(
+                <div>
+                    <a href={value}>{value}</a>
+                </div>
+            );
+        }
+        return contact;
+    }
+
+    return(
+        <div>
+            <a href={telecomNode.value}>{telecomNode.value}</a>
+        </div>
+    );
+}
+
+ReactDOM.render(<XMLForm/>, document.getElementById("modal-container"));
