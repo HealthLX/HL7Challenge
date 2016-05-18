5b86c3a927908e7059dfd6868ae420c13c50e3b2
Victor Velazco <vvelazco@VictorVelazco.local>
2016-05-18 11:43:50 -0500

    Adding last changes

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+//Make sure to include the JSX transpiler
+require('node-jsx').install();
+
 app.set('port', (process.env.PORT || 3000));
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+//Set view path
+app.set('views', path.join(__dirname, 'views'));
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+//Set up Routes for the application
+require('./app/routes/core-routes.js')(app);
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
