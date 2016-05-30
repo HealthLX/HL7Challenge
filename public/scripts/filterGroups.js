4dce57fc9b4951ca8d4fcff572ce29e395e5e85d
Israel Valdez <israel.valdez@teschglobal.com>
2016-05-30 13:40:32 -0500

    adding more filter codes for some common sections

--- public/scripts/filterGroups.js
+++ public/scripts/filterGroups.js
@@ -8,6 +8,22 @@ codes.immunizations="11369-6";
 codes.planOfCare="18776-5";
 codes.problemList="11450-4";
 codes.vitalSigns="8716-3";
+codes.encounters="46240-8";
+codes.procedures="47519-4";
+codes.results="30954-2";
+codes.socialHistory="29762-2";
+codes.healthConcerns="75310-3";
+codes.goals="61146-7";
+codes.interventions="62387-6";
+codes.healthStatusEvaluationsOutcomes="11383-7";
+codes.advanceDirectives="42348-3";
+codes.familyHistory="10157-6";
+codes.functionalStatus="47420-5";
+codes.medicalEquipment="46264-8";
+codes.insuranceProviders="48768-6";
+codes.reasonForReferral="42349-1";
+codes.reasonForVisit="29299-5";
+codes.instructions="69730-0";
 
 filters["Basics"]=
 {
